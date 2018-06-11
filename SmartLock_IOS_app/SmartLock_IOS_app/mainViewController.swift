73dea2b928f325f8da8f2627a527b7b6eee1bd5d
Tadej Strah <tadej.strah@gmail.com>
2018-06-11 22:34:32 +0200

    initial commit

---
+++ SmartLock_IOS_app/SmartLock_IOS_app/mainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SmartLock_IOS_app
+//
+//  Created by Tadej Strah on 11/06/2018.
+//  Copyright © 2018 Tadej Strah. All rights reserved.
+//
+
+import UIKit
+
+class mainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+
+}
+
